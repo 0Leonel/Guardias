7b133a9443bfa6cece2bcc46aaa75f6ed1b84dfd
0Leonel <leonel20112010@gmail.com>
2023-12-26 11:39:20 -0300

    init

--- src/core/components/appTable/appTable.jsx
+++ src/core/components/appTable/appTable.jsx
@@ -2,7 +2,6 @@ import { useData } from '../../services/useData'
 
 export const AppTable = () => {
     const {data} = useData();
-    console.log(data)
     const esFormatoHora =(cadena) =>{
         const expresionRegularHora = /^([01]\d|2[0-3]):[0-5]\d:[0-5]\d$/;
         return expresionRegularHora.test(cadena);
